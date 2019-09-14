7827e6fb39a64508b3e57519595f265312cb2469
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-09-14 11:14:59 -0400

    better order

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const taken = require( 'util' ).promisify( require( 'npm-taken' ) ) // This is what we use to find taken modules
 module.exports = require( 'an-array-of-english-words' ) // A list of words
   .filter( w => !!w.startsWith( 'yo' ) ) // Only words that start with "yo"
-  .filter( async w => !await taken( 'generator-' + w.substring( 2 ) ) ) // Check if it's taken
   .filter( e => !!e.substring( 2 ) ) // Remove any words that are only "yo"
   .map( e => e.substring( 2 ) ) // Removes "yo" from the start
+  .filter( async w => !await taken( `generator-${ w }` ) ) // Check if it's taken
   .map( w => `generator-${ w }` ) // Add "generator-" to the start
